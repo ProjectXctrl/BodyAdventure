87d66eb9f85737c12fd530f44a98ddfd3823f602
ProjectXctrl <105494744+ProjectXctrl@users.noreply.github.com>
2022-05-31 01:54:28 +0200

    Added smooth controls

--- js/main.js
+++ js/main.js
@@ -2,10 +2,23 @@ var World = require('three-world'),
     THREE = require('three'),
     Tunnel = require('./tunnel'),
     Player = require('./player'),
-    Asteroid = require('./kidneyStones'),
+    Virus = require('./Virus'),
     Shot = require('./shot')
+  
+    var lastTick = 0;
+    const MOVE_UP = 40; // e
+    const MOVE_DOWN = 38; // q
 
-var NUM_KIDSTONES = 13   //number of astroids generated at one time
+    var object;
+    var minY = Number.NEGATIVE_INFINITY;
+    var maxY = Number.POSITIVE_INFINITY;
+
+    var moveLeft = false;
+    var moveRight = false;
+    var moveUp = false;
+    var moveDown = false;
+
+var NUM_VIRUS = 8//number of virus generated at one time
 var LEVEL = 1  //level number
 
 //x,y & z positions of shots 
@@ -14,7 +27,14 @@ var py=25
 var pz=100;
 
 
+
+
 function render() {
+  //console.log("hu");
+
+  var t = performance.now();
+  var delta = t - lastTick;
+  update(delta / 1000);
  
   tunnel.update(cam.position.z)
   player.update()
@@ -73,12 +93,12 @@ if(LEVEL==2){
     }
   }
 
-  for(var i=0; i<NUM_KIDSTONES; i++) {
-    if(!kidstones[i].loaded) continue
+  for(var i=0; i<NUM_VIRUS; i++) {
+    if(!virus[i].loaded) continue
 
-    kidstones[i].update(cam.position.z)
-    if(player.loaded && player.bbox.isIntersectionBox(kidstones[i].bbox)) {   //if spaceship hits a virus
-      kidstones[i].reset(cam.position.z)
+    virus[i].update(cam.position.z)
+    if(player.loaded && player.bbox.isIntersectionBox(virus[i].bbox)) {   //if spaceship hits a virus
+      virus[i].reset(cam.position.z)
       health -= 20
       
       document.getElementById("health").textContent = health
@@ -92,11 +112,11 @@ if(LEVEL==2){
     
 
     for(var j=0; j<shots.length; j++) {
-      if(kidstones[i].bbox.isIntersectionBox(shots[j].bbox)) {  //if shot hits virus
+      if(virus[i].bbox.isIntersectionBox(shots[j].bbox)) {  //if shot hits virus
         var audio = new Audio('./song/alien3.wav');
         score += 10
         document.getElementById("score").textContent = score
-        kidstones[i].reset(cam.position.z)
+        virus[i].reset(cam.position.z)
         World.getScene().remove(shots[j].getMesh())
         shots.splice(j, 1)
         audio.play();
@@ -105,13 +125,20 @@ if(LEVEL==2){
     }
 
   }
+  lastTick=t;
 }
+
+
+
+
 
 
 var health = 100, score = 0
 
 World.init({ renderCallback: render, clearColor: "#620505"})
 var cam = World.getCamera()
+object=cam;
+//pitch.add(object);
 
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
 World.add(directionalLight )
@@ -123,11 +150,11 @@ World.add(tunnel.getMesh())
 var player = new Player(cam)
 World.add(cam)
 
-var kidstones = [], shots = []
+var virus = [], shots = []
 
-for(var i=0;i<NUM_KIDSTONES; i++) {
-  kidstones.push(new Asteroid(Math.floor(Math.random() * 5) + 1))
-  World.add(kidstones[i].getMesh())
+for(var i=0;i<NUM_VIRUS; i++) {
+  virus.push(new Virus(Math.floor(Math.random() * 5) + 1))
+  World.add(virus[i].getMesh())
 }
 
 World.getScene().fog = new THREE.FogExp2("#620505", 0.00110)
@@ -137,29 +164,41 @@ World.start()
 //key is pressed and let go
 window.addEventListener('keyup', function(e) { 
   switch(e.keyCode) {
-    case 32: // if the key is Space, shoot
-      var audio = new Audio('./song/hit.mp3');
-      audio.play();
-      var shipPosition = cam.position.clone()
-      shipPosition.sub(new THREE.Vector3(px, py, pz))
-      var shot = new Shot(shipPosition)
-      shots.push(shot)
-      World.add(shot.getMesh())
-    break
+      case 32: // if the key is Space, shoot
+        var audio = new Audio('./song/hit.mp3');
+        audio.play();
+        var shipPosition = cam.position.clone()
+        shipPosition.sub(new THREE.Vector3(px, py, pz))
+        var shot = new Shot(shipPosition)
+        shots.push(shot)
+        World.add(shot.getMesh())
+      break
 
-    case 81: // if the key is Q, switch to first person
-      px=0;
-      py=10;
-      pz=12;
-      player.firstPerson();
-    break
+      case 37: /*left*/
+      case 65: /*A*/ moveLeft = false; break;
 
-    case 69: // if the key is E, switch to third person
-      px=0
-      py=25
-      pz=100;
-      player.thirdPerson();
-    break
+      case 39: /*right*/
+      case 68: /*D*/ moveRight = false; break;
+
+      case 83: /*up*/
+      case MOVE_UP: moveUp = false; break;
+  
+      case 87: /*down*/
+      case MOVE_DOWN: moveDown = false; break;
+
+      case 81: // Q
+        px=0;
+        py=10;
+        pz=12;
+        player.firstPerson();
+      break
+
+      case 69: // E
+        px=0
+        py=25
+        pz=100;
+        player.thirdPerson();
+      break
 
   }
 })
@@ -178,22 +217,51 @@ document.addEventListener("mousedown", function(e) {  //when mouse is clicked, s
 
 //when key is pressed
 window.addEventListener('keydown', function(e) {
-  if(e.key === "ArrowLeft"&& cam.position.x>-55) {   //move left
+  switch (e.keyCode) {
 
-    cam.position.x -= 5
-  } 
-  else if(e.key === "ArrowRight"&& cam.position.x<55 ) {   //move right
+    case 37: /*left*/
+    case 65: /*A*/ moveLeft = true; break;
+
+    case 39: /*right*/
+    case 68: /*D*/ moveRight = true; break;
+
+    case 83: /*up*/
+    case MOVE_UP: moveUp = true; break;
+
+    case 87: /*down*/
+    case MOVE_DOWN: moveDown = true; break;
+
+
+  }
+});
+
+
+function update(delta) {
+ 
+
+
+ 
+    if ((moveLeft || moveRight)  ) {
    
-    cam.position.x += 5 ;
-  }
+      const x_contrib =  (Number(moveLeft) - Number(moveRight)) ;
+      var nextRightPos = Math.max(minY, Math.min(maxY, (object.position.x - x_contrib)+1));
+      var nextLeftPos = Math.max(minY, Math.min(maxY, (object.position.x - x_contrib)-1));
 
-  if(e.key === "ArrowUp" && cam.position.y<60) {   //move up
-    cam.position.y += 5
-  } 
-  
-  else if(e.key === "ArrowDown" && cam.position.y>-20) {   //move down
+      if((nextLeftPos>-54 && nextRightPos<54)){
+      object.position.x = Math.max(minY, Math.min(maxY, object.position.x - x_contrib));
+      }
     
-    cam.position.y -= 5
-  }
-})
+    }
+ 
+      if (moveUp || moveDown ) {
+        const y_contrib =  (Number(moveUp) - Number(moveDown)) ;
+        var nextUpPos = Math.max(minY, Math.min(maxY, (object.position.y - y_contrib)+1));
+        var nextDownPos = Math.max(minY, Math.min((maxY, object.position.y - y_contrib)-1));
+
+        if((nextDownPos>=-20 && nextUpPos<=60)){
+        object.position.y = Math.max(minY, Math.min(maxY, object.position.y - y_contrib));
+        }
+
+      }
+}
 
