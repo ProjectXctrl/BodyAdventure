b9434fdd0afe92a218d84d22286efa465193213a
ProjectXctrl <105494744+ProjectXctrl@users.noreply.github.com>
2022-06-10 09:10:14 +0200

    Update Virus.js

--- js/Virus.js
+++ js/Virus.js
@@ -13,9 +13,9 @@ var Virus = function(rockType) {
   mesh.velocity = Math.random() * 4 + 2
   mesh.vRotation = new THREE.Vector3(Math.random(), Math.random(), Math.random())
 
-  this.bbox = new THREE.Box3()
+  this.bbox = new THREE.Box3() //collision-detection box
 
-  loader.load('models/covid'  + '.obj', function(obj) {
+  loader.load('models/covid'  + '.obj', function(obj) {  //load blender virus model
     obj.traverse(function(child) {
       if(child instanceof THREE.Mesh) {
         child.material = rockMtl
@@ -29,16 +29,16 @@ var Virus = function(rockType) {
     mesh.add(obj)
     mesh.position.set(-50 + Math.random() * 100, -50 + Math.random() * 100, -1500 - Math.random() * 1500)
 
-    self.bbox.setFromObject(obj)
+    self.bbox.setFromObject(obj)  //set size of collision detection box based on virus size
     self.loaded = true
   })
 
-  this.reset = function(z) {
+  this.reset = function(z) {    //reset them back where they come from
     mesh.velocity = Math.random() * 2 + 2
     mesh.position.set(-50 + Math.random() * 100, -50 + Math.random() * 100, z - 1500 - Math.random() * 1500)
   }
 
-  this.update = function(z) {
+  this.update = function(z) {   //update current position
     mesh.position.z += mesh.velocity
     mesh.rotation.x += mesh.vRotation.x * 0.02;
     mesh.rotation.y += mesh.vRotation.y * 0.02;
@@ -51,7 +51,7 @@ var Virus = function(rockType) {
     }
   }
 
-  this.getMesh = function() {
+  this.getMesh = function() {   // to get the finished product (viruses)
     return mesh
   }
 
